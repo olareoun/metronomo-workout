b38db3ed0ab41e0fa19c46c489ece15add6a551d
olareoun <victor.miguez.martinez@gmail.com>
2014-03-18 11:35:05 +0100

    bla

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,9 +1,32 @@
 var APP = {};
+var MT = {};
 
 APP.init = function(){
+
+    $( "#slider" ).slider({
+      value:20,
+      min: 20,
+      max: 240,
+      step: 20,
+      slide: function( event, ui ) {
+        $( "#bpm" ).val( ui.value );
+      }
+    });
+    $( "#bpm" ).val( $( "#slider" ).slider( "value" ) );
+
+
+    $('#start').on('click', MT.play);
+};
+
+MT.play = function(){
+    $('#status').html("started");
+    var sounds = [0, 0, 0, 1];
+    var index = 0;
     setInterval(function(){
-        $("#audio")[0].play();
-    }, 1000);
+        if (index >= sounds.length) index = 0;
+        $("#audio" + sounds[index])[0].play();
+        index++;
+    }, 60000 / $('#bpm').val());
 };
 
 $(function() {
